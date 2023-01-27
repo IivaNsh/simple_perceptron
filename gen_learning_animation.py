363d1ad3f3bed12fb87730bb99a0cdb11f2416a0
IivaNsh <ivansher0226@gmail.com>
2023-01-27 18:21:16 +0300

    adding learning animation

--- gen_learning_animation.py
+++ gen_learning_animation.py
@@ -1,7 +1,7 @@
 from perceptron import *
 import point_generator
 import matplotlib.pyplot as plt
-
+from matplotlib.animation import PillowWriter
 
 print("--------<prepating data>--------")
 
@@ -20,31 +20,11 @@ ts = np.concatenate((np.array([[0]]*len(ps1)),np.array([[1]]*len(ps2))))
 
 
 
-
-
-
-model1 = Perceptron(2, 2)
-
-print("-------<start learning>--------")
-
-iterations = 3000
-for _ in range(iterations):
-    for i in range(len(ps)):
-        model1.fit(ps[i], ts[i])
-    
-
-print("-------<end learning>--------")
-
-
-
-
-
-
-
+metadata = dict(title="learing_animation", atrist="ivn")
+write = PillowWriter(fps=25, metadata=metadata)
 
 
 fig, ax = plt.subplots()
-
 
 xs_o = ps1.transpose()[0]
 ys_o = ps1.transpose()[1]
@@ -58,18 +38,30 @@ ax.plot(xs_t, ys_t, "^", color="blue")
 
 
 
-
-
-
-
 resolution = 100
 y, x = np.meshgrid(np.linspace(-10, 10, resolution), np.linspace(-10, 10, resolution))
 
-z = [[model1.predict([x[y_i][x_i], y[y_i][x_i]])[0] for x_i in range(resolution)] for y_i in range(resolution)] 
 
-z_min, z_max = -np.abs(z).max(), np.abs(z).max()
 
-ax.pcolormesh(x, y, z, cmap='RdYlGn')
+model1 = Perceptron(2, 2)
+
+print("-------<start learning>--------")
+
+with write.saving(fig, "learing_animation.gif", 100):
+    iterations = 2500
+    for _ in range(iterations):
+        for i in range(len(ps)):
+            model1.fit(ps[i], ts[i])
+
+        if (_%10==0):
+            z = [[model1.predict([x[y_i][x_i], y[y_i][x_i]])[0] for x_i in range(resolution)] for y_i in range(resolution)] 
+            z_min, z_max = -np.abs(z).max(), np.abs(z).max()
+            ax.pcolormesh(x, y, z, cmap='RdYlGn')
+            write.grab_frame()
+
+    
+
+print("-------<end learning>--------")
 
 
 
